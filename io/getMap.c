f4b9157dba9d2c33f732e8cd047f766d7964cb2c
Charles Thompson <chmithbiz@gmail.com>
2016-09-08 21:03:57 -0400

    Uploaded file

--- io/getMap.c
+++ io/getMap.c
@@ -1,1 +1,39 @@
+/* getMap - reads a map from the specified file stream, storing it in the global
+            array
 
+ARGUMENTS:
+
+FILE *file - the filestream being read
+
+VARIABLES:
+
+*/
+
+#include <getMap.h>
+
+int getMap(FILE *file)
+{
+    natural index = INDEXINIT;
+
+    FLETCHER16_INIT();
+
+    if(!(ARRAY_INIT3D(READBYTE(file), READBYTE(file), READBYTE(file))))
+    {
+        perror(MSG_PERROR);
+        return errno;
+    }
+
+    while(++index < ARRAY_LENGTH)
+    {
+        ARRAY_ACCESS(index) = READBYTE(file);
+        FLETCHER16_ADD(ARRAY_ACCESS(index));
+    }
+
+    if(fletcherA != READBYTE(file) || fletcherB != READBYTE(file))
+    {
+        printf(MSG_BADFLETCHER);
+        return err_badFletcher;
+    }
+
+    return none;
+}
