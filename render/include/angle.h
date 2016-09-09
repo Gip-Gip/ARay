919b509b33a14f2656382c449489f2b74dc432dc
Charles Thompson <chmithbiz@gmail.com>
2016-09-09 11:07:08 -0400

    Changes ANGLEZ_INIT to SLOPEZ_INIT

--- render/include/angle.h
+++ render/include/angle.h
@@ -9,7 +9,7 @@ DEFINITIONS:
 ANGLEMAX - the maximum angle possible
 ANGLEX_INIT - the initial angle for the X angle array
 ANGLEY_INIT - the initial angle for the Y angle array
-ANGLEZ_INIT - the initial angle for the Z angle array
+SLOPEZ_INIT - the initial slope for the Z angle array
 
 MACROS:
 
@@ -18,4 +18,4 @@ MACROS:
 #define ANGLEMAX 8.0
 #define ANGLEX_INIT 0
 #define ANGLEY_INIT 6
-#define ANGLEZ_INIT 0
+#define SLOPEZ_INIT 1
