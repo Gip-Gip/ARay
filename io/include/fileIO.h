e0e84cf243d721dc84feace9c3458b1a75ddf2f6
Charles Thompson <chmithbiz@gmail.com>
2016-09-08 21:06:48 -0400

    Uploaded file

--- io/include/fileIO.h
+++ io/include/fileIO.h
@@ -1,1 +1,23 @@
+/* io_fileIO.h - includes files and defines things needed for file IO
 
+ENUMS:
+
+TYPEDEFS:
+
+DEFINITIONS:
+
+READMODE - the mode used for reading
+
+MACROS:
+
+READBYTE - reads a byte from the specified file
+WRITEBYTE - writes to the specified file with the given byte
+
+*/
+
+#include <stdio.h>
+
+#define READMODE "rb"
+
+#define READBYTE(file) fgetc(file)
+#define WRITEBYTE(file, data) fputc(data, file)
---
+++ io/include/getMap.h
@@ -0,0 +1,16 @@
+/* io_getMap.h - includes files needed by io_getMap.c
+
+ENUMS:
+
+TYPEDEFS:
+
+DEFINITIONS:
+
+MACROS:
+
+*/
+
+#include <common.h>
+#include <fileIO.h>
+#include <messages.h>
+#include <fletcher.h>
