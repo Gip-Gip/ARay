52f60f29b72b6aea52f1792d1767e4b511868e73
Charles Thompson <chmithbiz@gmail.com>
2016-09-11 15:36:24 -0400

    Fixed a simple wall detection buf

--- render/drawScrn.c
+++ render/drawScrn.c
@@ -4,6 +4,12 @@ ARGUMENTS:
 
 VARIABLES:
 
+natural index - the index used to go through the screen and the angle arrays
+rational rayX - the ray's x coordnate
+rational rayY - the ray's y coordnate
+rational rayZ - the ray's x coordnate
+mapunit wall - the wall hit by the ray
+
 */
 
 #include <drawScrn.h>
@@ -22,28 +28,24 @@ void drawScrn()
         for(;ARRAY_ACCESS3D(
             R2N(rayX), R2N(rayY), R2N(rayZ)) == AIR; wall = AIR)
         {
-            rayX += angleArray_X[index];
             if(AIR != (wall = ARRAY_ACCESS3D(
-                R2N(rayX), R2N(rayY), R2N(rayZ))))
+                R2N((rayX += angleArray_X[index])), R2N(rayY), R2N(rayZ))))
             {
                 if(angleArray_X[index] > 0)
                     getTextr(xHitP, rayY, rayZ, index, wall);
                 else getTextr(xHitN, rayY, rayZ, index, wall);
             }
 
-            rayY += angleArray_Y[index];
-            if(wall == AIR && AIR != (wall = ARRAY_ACCESS3D(
-            R2N(rayX), R2N(rayY), R2N(rayZ))))
+            else if(AIR != (wall = ARRAY_ACCESS3D(
+                R2N(rayX), R2N((rayY += angleArray_Y[index])), R2N(rayZ))))
             {
                 if(angleArray_Y[index] > 0)
                     getTextr(yHitP, rayX, rayZ, index, wall);
                 else getTextr(yHitN, rayX, rayZ, index, wall);
             }
 
-            rayZ += angleArray_Z[index];
-
-            if(wall == AIR && AIR != (wall = ARRAY_ACCESS3D(
-            R2N(rayX), R2N(rayY), R2N(rayZ))))
+            else if(AIR != (wall = ARRAY_ACCESS3D(
+                R2N(rayX), R2N(rayY), R2N((rayZ += angleArray_Z[index])))))
             {
                 if(angleArray_Z[index] > 0)
                     getTextr(zHitP, rayX, rayY, index, wall);
