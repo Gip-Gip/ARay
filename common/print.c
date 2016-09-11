8d178b3226058ea7f219b271d721ade193b4a50c
Charles Thompson <chmithbiz@gmail.com>
2016-09-11 11:32:02 -0400

    Added the ability to print numbers

--- common/print.c
+++ common/print.c
@@ -7,6 +7,9 @@ char *format - the format of the message being printed
 VARIABLES:
 
 va_list arglist - the variable-argument list
+string history - the previous print
+bool freeHistory - if set, history will be freed
+natural num - used for natural conversions
 
 */
 
@@ -15,7 +18,9 @@ va_list arglist - the variable-argument list
 void print(char *format, ...)
 {
     va_list arglist;
-    char *history;
+    string history;
+    bool freeHistory = false;
+    natural num = 1;
 
     va_start(arglist, format);
 
@@ -24,9 +29,26 @@ void print(char *format, ...)
         if(*format == PRINT_STYLDELM) switch(*(++format))
         {
             case(PRINT_STYLSTR):
-                print(history = va_arg(arglist, char *));
-                break;
+                if(freeHistory)
+                {
+                    freeHistory = false;
+                    free(history);
+                }
 
+                print(history = va_arg(arglist, string));
+                break;
+            case(PRINT_STYLNATR):
+                if(freeHistory) free(history);
+
+                num = va_arg(arglist, natural);
+                history =
+                    calloc((num?floor(log10(num)):STRALLOC) + STRALLOC,
+                    sizeof(character));
+                sprintf(history, F_NATURAL, num);
+                print(history);
+
+                freeHistory = true;
+                break;
             case(PRINT_STYLPREV):
                 print(history);
                 break;
