88f4b7f23fc37ab5ecc123eb903b8427ff4ef5fb
Gip-Gip <chmithbiz@gmail.com>
2016-10-01 11:59:27 -0400

    Added the license, build and verbose arguments

--- common/args.c
+++ common/args.c
@@ -14,7 +14,10 @@ string args[] =
 {
     "-c", "--config", "set the config file",
     "-h", "--help", "displays this message and exits",
-    "-l", "--log", "sets the logfile",
+    "-l", "--log", "the logfile",
+    "-Bi", "--build-script", "the build script, if building",
+    "-Bo", "--build-product", "the file that will be written to, if building",
+    "-v", "--verbose", "print extra debugging messages",
     "-y", "--overwrite", "overwrites files without complaining",
     NULL,
 };
