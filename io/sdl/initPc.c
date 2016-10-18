225e0366d2e7c45d11b12198d291180190bdf279
Gip-Gip <chmithbiz@gmail.com>
2016-10-18 10:59:54 -0400

    Fixed a comment

--- io/sdl/initPc.c
+++ io/sdl/initPc.c
@@ -1,4 +1,4 @@
-/* initPc - controls the player
+/* initPc - inititializes the player's controls, along with a few values
 
 ARGUMENTS:
 
@@ -7,6 +7,7 @@ VARIABLES:
 rational playerX - the player's X position
 rational playerY - the player's Y position
 rational playerZ - the player's Z position
+natural playerRot - the player's rotation
 
 */
 
